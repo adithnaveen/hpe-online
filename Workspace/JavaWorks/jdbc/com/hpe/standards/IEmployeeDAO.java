df620f257a2fcb0365c20fc0a56fc429714a1036
Naveen <adith.naveen@gmail.com>
2019-08-28 21:18:14 +0530

    Added post day 12

---
+++ Workspace/JavaWorks/jdbc/com/hpe/standards/IEmployeeDAO.java
@@ -0,0 +1,22 @@
+package com.hpe.standards;
+
+import java.util.List;
+
+import com.hpe.beans.Employee;
+
+public interface IEmployeeDAO {
+//	1. insert 
+	public boolean insertEmployee(Employee employee); 
+	
+//	2. update 
+	public boolean updateEmployee(int empId, String email); 
+	
+//	3. delete 
+	public boolean deleteEmployee(int empId); 
+	
+//	4. select - one
+	public Employee getEmployee(int empId); 
+	
+//	5. select - all
+	public List<Employee> getAllEmployees(); 
+}
